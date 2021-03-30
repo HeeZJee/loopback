a22731e96b70030ee758e36c0e3f0c2a286c7313
HeeZJee <hafeezghanchi927@gmail.com>
2021-03-31 02:19:32 +0500

    added admin user on every startup

--- server/server.js
+++ server/server.js
@@ -32,7 +32,22 @@ boot(app, __dirname, function (err) {
   if (require.main === module) app.start();
 });
 
-// console.log("server",Object.keys(app.models.User))
+app.models.user.find((err, result) => {
+  if (err) { console.warn(err) }
+  else {
+    if (result.length === 0) {
+      const user = {
+        email: 'admin@gmail.com',
+        password: 'abc123',
+        username: 'admin'
+      }
+      app.models.user.create(user, (err, result) => {
+        err ? console.warn("Error", err) : console.log("Result", result)
+      })
+    }
+
+  }
+})
 
 app.models.user.afterRemote("create", (ctx, user, cb) => {
   console.log("New User \n", user);
@@ -44,7 +59,12 @@ app.models.user.afterRemote("create", (ctx, user, cb) => {
       birthDate: "1998-03-19T15:19:13.057Z",
       createdAt: new Date(),
     },
-    (err, result) => ((err && !result) ? console.error("Error:",err) : console.log("New Profile: \n",result))    
+    (err, result) => ((err && !result) ? console.error("Error:", err) : console.log("New Profile: \n", result))
   );
   cb()
 });
+
+
+app.middleware('auth', loopback.token({
+  model: app.models.customAccessToken,
+}))
